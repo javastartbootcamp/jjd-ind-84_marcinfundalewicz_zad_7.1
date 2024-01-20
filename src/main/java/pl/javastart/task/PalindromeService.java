e81b360418d6f109ad7acbae7acbc046d46a44ef
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-20 20:49:39 +0100

    commit 1

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,6 +4,21 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+        int counter = 0;
+        if (array == null) {
+            return false;
+        }
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] == array[(array.length - 1) - i]) {
+                counter++;
+            }
+        }
+        if (counter >= array.length / 2 && array.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
+
